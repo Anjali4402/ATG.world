e06ba8b3fd64695cf136ce17c5a4fbdb22fc88a3
Anjali <anjalinegian2002@gmail.com>
2023-09-24 16:31:01 +0530

    commit for image

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Navbar from './component/Navbar'
 import MainImage from './component/MainImage'
 import WebsiteContent from './component/WebsiteContent'
-import ModalDialog from './component/ModalDialog'
+import ModalDialog from './component/ModalDialog';
 
 function App() {
   return (
